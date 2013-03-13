15519ef218bfde73b7322f4e8c95f2368a250f5c
jorne raeymaekers <jorne.raeymaekers@hotmail.com>
2013-03-13 12:07:10 +0100

    roemel

---
+++ AndroidTeamF/src/main/java/teamf/view/ViewTrips.java
@@ -0,0 +1,29 @@
+package teamf.view;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jorne
+ * Date: 13/03/13
+ * Time: 12:06
+ * To change this template use File | Settings | File Templates.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.project.TeamFAndroid.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jeroen
+ * Date: 12/03/13
+ * Time: 14:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class ViewTrips extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trips);
+    }
+}
+
