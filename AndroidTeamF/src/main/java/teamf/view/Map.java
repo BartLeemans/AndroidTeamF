e9b7d52090d73abac7080502ad03a5550187c883
BART.LEEMANS <bart.leemans@student.kdg.be>
2013-03-15 12:21:48 +0100

    maps

--- AndroidTeamF/src/main/java/teamf/view/Map.java
+++ AndroidTeamF/src/main/java/teamf/view/Map.java
@@ -1,6 +1,8 @@
 package teamf.view;
 
 import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import com.project.TeamFAndroid.R;
 
@@ -14,8 +16,18 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import teamf.controller.ServerCaller;
+import teamf.model.StopPlaats;
+import teamf.model.Trip;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Map extends Activity {
+    private ServerCaller sc = ServerCaller.getInstance();
+    private Trip detail;
+    private List<StopPlaats> plaatsen;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         String message = "";
@@ -26,16 +38,23 @@ public class Map extends Activity {
             FragmentManager fragmentManager = getFragmentManager();
             MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
             GoogleMap googleMap = mapFragment.getMap();
-            LatLng sfLatLng = new LatLng(37.7750, -122.4183);
-            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+            detail = (Trip)getIntent().getSerializableExtra("Trip");
+            plaatsen = new ArrayList<StopPlaats>(sc.getStops());
+
+            final Geocoder geocoder = new Geocoder(getBaseContext());
+            List<Address> GeocodeResponse = geocoder.getFromLocationName(plaatsen.get(0).getAdres(), 10);
+            LatLng position = new LatLng(GeocodeResponse.get(0).getLatitude(), GeocodeResponse.get(0).getLongitude());
             googleMap.addMarker(new MarkerOptions()
-                    .position(sfLatLng)
-                    .title("San Francisco")
-                    .snippet("Population: 776733")
+                    .position(position)
+                    .title("Paris")
+                    .snippet("Population: kaka")
                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
             googleMap.getUiSettings().setCompassEnabled(true);
             googleMap.getUiSettings().setZoomControlsEnabled(true);
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sfLatLng, 10));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 10));
+
 
         } catch (Exception e) {
             message = e.getMessage();
