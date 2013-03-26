fd9d77dfbe43b38853d2d8b542bc41c0667e71f2
Stijn Struys <stijn.struys@student.kdg.be>
2013-03-26 15:05:40 +0100

    nomap

---
+++ AndroidTeamF/src/main/java/teamf/view/NoMap.java
@@ -0,0 +1,19 @@
+package teamf.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.project.TeamFAndroid.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stijn
+ * Date: 26-3-13
+ * Time: 15:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class NoMap extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.noMap);
+    }
+}
